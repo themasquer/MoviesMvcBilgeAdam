bc97c2b260fce5ad18fc76da20f440ce2c41bdaf
Ankbilgeadam@outlook.com <Ankbilgeadam@outlook.com>
2021-03-06 19:13:30 +0300

    v1

--- 036_MoviesMvcBilgeAdam/Controllers/MoviesReportController.cs
+++ 036_MoviesMvcBilgeAdam/Controllers/MoviesReportController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web.Mvc;
 using _036_MoviesMvcBilgeAdam.Contexts;
@@ -20,15 +22,84 @@ namespace _036_MoviesMvcBilgeAdam.Controllers
         }
 
         // GET: MoviesReport
-        public ActionResult Index(int? OnlyMatchedValue = null)
+        //public ActionResult Index(int? OnlyMatchedValue = null)
+        public ActionResult Index(MoviesReportIndexViewModel moviesReport)
         {
+            IQueryable<MovieReportInnerJoinModel> innerJoinQuery;
+            IQueryable<MovieReportLeftOuterJoinModel> leftOuterJoinQuery;
+
             List<MovieReportInnerJoinModel> innerJoinList = null;
             List<MovieReportLeftOuterJoinModel> leftOuterJoinList = null;
 
-            if ((OnlyMatchedValue ?? 1) == 1) // only matched, inner join
-                innerJoinList = movieReportService.GetInnerJoinQuery().ToList();
+            //if ((OnlyMatchedValue ?? 0) == 1) // only matched, inner join
+            if (moviesReport.OnlyMatchedValue == 1)
+            {
+                innerJoinQuery = movieReportService.GetInnerJoinQuery();
+                if (!string.IsNullOrWhiteSpace(moviesReport.MovieName))
+                    innerJoinQuery = innerJoinQuery.Where(model => model.MovieName.ToUpper().Contains(moviesReport.MovieName.ToUpper().Trim()));
+                if (moviesReport.ProductionYears != null && moviesReport.ProductionYears.Count > 0)
+                    innerJoinQuery = innerJoinQuery.Where(model => moviesReport.ProductionYears.Contains(model.MovieProductionYear));
+                double boxOfficeReturn1;
+                double boxOfficeReturn2;
+                if (!string.IsNullOrWhiteSpace(moviesReport.BoxOfficeReturn1))
+                {
+                    if (double.TryParse(moviesReport.BoxOfficeReturn1.Trim().Replace(",", "."), NumberStyles.Any, new CultureInfo("en"), out boxOfficeReturn1))
+                        innerJoinQuery = innerJoinQuery.Where(model => model.MovieBoxOfficeReturnValue >= boxOfficeReturn1);
+                }
+                if (!string.IsNullOrWhiteSpace(moviesReport.BoxOfficeReturn2))
+                {
+                    if (double.TryParse(moviesReport.BoxOfficeReturn2.Trim().Replace(",", "."), NumberStyles.Any, new CultureInfo("en"), out boxOfficeReturn2))
+                        innerJoinQuery = innerJoinQuery.Where(model => model.MovieBoxOfficeReturnValue <= boxOfficeReturn2);
+                }
+                DateTime reviewDate1;
+                DateTime reviewDate2;
+                if (!string.IsNullOrWhiteSpace(moviesReport.ReviewDate1))
+                {
+                    reviewDate1 = DateTime.Parse(moviesReport.ReviewDate1, new CultureInfo("en"));
+                    innerJoinQuery = innerJoinQuery.Where(model => model.ReviewDateValue >= reviewDate1);
+                }
+                if (!string.IsNullOrWhiteSpace(moviesReport.ReviewDate2))
+                {
+                    reviewDate2 = DateTime.Parse(moviesReport.ReviewDate2, new CultureInfo("en"));
+                    innerJoinQuery = innerJoinQuery.Where(model => model.ReviewDateValue <= reviewDate2);
+                }
+
+                innerJoinList = innerJoinQuery.ToList();
+            }
             else // not only matched, left outer join
-                leftOuterJoinList = movieReportService.GetLeftOuterJoinQuery().ToList();
+            {
+                leftOuterJoinQuery = movieReportService.GetLeftOuterJoinQuery();
+                if (!string.IsNullOrWhiteSpace(moviesReport.MovieName))
+                    leftOuterJoinQuery = leftOuterJoinQuery.Where(model => model.MovieName.ToUpper().Contains(moviesReport.MovieName.ToUpper().Trim()));
+                if (moviesReport.ProductionYears != null && moviesReport.ProductionYears.Count > 0)
+                    leftOuterJoinQuery = leftOuterJoinQuery.Where(model => moviesReport.ProductionYears.Contains(model.MovieProductionYear));
+                double boxOfficeReturn1;
+                double boxOfficeReturn2;
+                if (!string.IsNullOrWhiteSpace(moviesReport.BoxOfficeReturn1))
+                {
+                    if (double.TryParse(moviesReport.BoxOfficeReturn1.Trim().Replace(",", "."), NumberStyles.Any, new CultureInfo("en"), out boxOfficeReturn1))
+                        leftOuterJoinQuery = leftOuterJoinQuery.Where(model => model.MovieBoxOfficeReturnValue >= boxOfficeReturn1);
+                }
+                if (!string.IsNullOrWhiteSpace(moviesReport.BoxOfficeReturn2))
+                {
+                    if (double.TryParse(moviesReport.BoxOfficeReturn2.Trim().Replace(",", "."), NumberStyles.Any, new CultureInfo("en"), out boxOfficeReturn2))
+                        leftOuterJoinQuery = leftOuterJoinQuery.Where(model => model.MovieBoxOfficeReturnValue <= boxOfficeReturn2);
+                }
+                DateTime reviewDate1;
+                DateTime reviewDate2;
+                if (!string.IsNullOrWhiteSpace(moviesReport.ReviewDate1))
+                {
+                    reviewDate1 = DateTime.Parse(moviesReport.ReviewDate1, new CultureInfo("en"));
+                    leftOuterJoinQuery = leftOuterJoinQuery.Where(model => model.ReviewDateValue >= reviewDate1);
+                }
+                if (!string.IsNullOrWhiteSpace(moviesReport.ReviewDate2))
+                {
+                    reviewDate2 = DateTime.Parse(moviesReport.ReviewDate2, new CultureInfo("en"));
+                    leftOuterJoinQuery = leftOuterJoinQuery.Where(model => model.ReviewDateValue <= reviewDate2);
+                }
+
+                leftOuterJoinList = leftOuterJoinQuery.ToList();
+            }
 
             List<SelectListItem> onlyMatchedSelectListItems = new List<SelectListItem>()
             {
@@ -46,13 +117,25 @@ namespace _036_MoviesMvcBilgeAdam.Controllers
                 }
             };
 
+            List<SelectListItem> productionYearSelectListItems = new List<SelectListItem>();
+            for (int year = DateTime.Now.Year + 1; year >= 1930; year--)
+            {
+                productionYearSelectListItems.Add(new SelectListItem()
+                {
+                    Value = year.ToString(),
+                    Text = year.ToString()
+                });
+            }
+
             MoviesReportIndexViewModel viewModel = new MoviesReportIndexViewModel()
             {
                 InnerJoinList = innerJoinList,
                 LeftOuterJoinList = leftOuterJoinList,
 
                 //OnlyMatchedSelectList = new SelectList(onlyMatchedSelectListItems, "Value", "Text", "1")
-                OnlyMatchedSelectList = new SelectList(onlyMatchedSelectListItems, "Value", "Text")
+                OnlyMatchedSelectList = new SelectList(onlyMatchedSelectListItems, "Value", "Text"),
+
+                ProductionYearMultiSelectList = new MultiSelectList(productionYearSelectListItems, "Value", "Text")
 
                 //, OnlyMatchedValue = 1
             };
--- 036_MoviesMvcBilgeAdam/Models/MoviesReportIndexViewModel.cs
+++ 036_MoviesMvcBilgeAdam/Models/MoviesReportIndexViewModel.cs
@@ -13,6 +13,26 @@ namespace _036_MoviesMvcBilgeAdam.Models
 
         [DisplayName("Only Matched")]
         //public int OnlyMatchedValue { get; set; }
-        public int OnlyMatchedValue { get; set; } = 1;
+        public int OnlyMatchedValue { get; set; } = 0;
+
+        [DisplayName("Movie Name")]
+        public string MovieName { get; set; }
+
+        public MultiSelectList ProductionYearMultiSelectList { get; set; }
+
+        [DisplayName("Production Years")]
+        public List<string> ProductionYears { get; set; }
+
+        [DisplayName("Box Office Return")]
+        //public double? BoxOfficeReturn1 { get; set; }
+        public string BoxOfficeReturn1 { get; set; }
+
+        //public double? BoxOfficeReturn2 { get; set; }
+        public string BoxOfficeReturn2 { get; set; }
+
+        [DisplayName("Review Date")]
+        public string ReviewDate1 { get; set; }
+
+        public string ReviewDate2 { get; set; }
     }
 }
